5701e2df94870da2d97e1e210836ccf77a4f7682
mohammadxxali <ali_mohammad@t-online.de>
2023-06-11 16:07:47 +0200

    add draw function

--- app.js
+++ app.js
@@ -46,3 +46,18 @@ function checkHitWall() {
         headTail.y = 0
     }
 }
+
+function draw() {
+    createRect(0, 0, canvas.width, canvas.height, "black")
+    createRect(0, 0, canvas.width, canvas.height)
+
+    for (let i = 0; i < snake.tail.length; i++) {
+        createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5,
+            snake.size - 5, snake.size - 5, "white")
+    }
+
+    canvasContext.font = "20px Arial"
+    canvasContext.fillStyle = "#00FF42"
+    canvasContext.fillText("Score: " + (snake.tail.length - 1), canvas.width - 120, 18)
+    createRect(apple.x, apple.y, apple.size, apple.size, apple.color)
+}
