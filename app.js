4361b84be51550fa7efd1c0b63952d4b3ff0db57
mohammadxxali <ali_mohammad@t-online.de>
2023-06-12 22:24:42 +0200

    add apple class

--- app.js
+++ app.js
@@ -125,5 +125,30 @@ class Snake {
     }
 }
 
+class Apple{
+    constructor(){
+        let isTouching
+        
+        while (true) {
+            isTouching = false;
+            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
+            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
+            
+            for (let i = 0; i < snake.tail.length; i++) {
+                if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
+                    isTouching = true
+                }
+            }
 
+            this.size = snake.size
+            this.color = "red"
 
+            if (!isTouching) {
+                break;
+            }
+        }
+    }
+}
+
+const snake = new Snake(20,20,20);
+let apple = new Apple();
